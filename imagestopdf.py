96c58efca3065c04c9fbbda3b7b7e23f007b703b
Ercan Berber <30718662+ercanbrbr@users.noreply.github.com>
2020-02-29 13:12:30 +0300

    Update imagestopdf.py

--- imagestopdf.py
+++ imagestopdf.py
@@ -1,8 +1,6 @@
 import os
 from PIL import Image
 import re
-import time
-from tqdm import tqdm
 # I found this function in stackoverflow. It is a sorting algorithm. OS sorts files like this 1,10,...,2,20,...,3 etc.
 # This function solves that. Thanks to user user136036
 def sorted_alphanumeric(data):
@@ -28,7 +26,7 @@ def lookforDir(basepath):
 # @basepath; location from user
 # @liste; list of a folders inside the root.
 def openDir(liste,basepath):
-    for direc in tqdm(liste):
+    for direc in liste:
         newpath=f"{basepath}/{direc}"
         scanfile(newpath,basepath,direc)
 
